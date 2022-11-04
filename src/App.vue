bb37189d9b465ba05f8f27480835e2dce1f440bb
xrbyzdys <1244032136@qq.com>
2022-11-05 02:51:07 +0800

    replace image source

--- src/App.vue
+++ src/App.vue
@@ -107,23 +107,19 @@ onMounted(() => {
 <template>
   <div flex flex-col w-full h-full>
     <div text-44px text-center w-full color="#000" fw-600 h-60px flex items-center justify-center mt-10px>
-      兔了个兔
+      Triple elimination
     </div>
-    <div ref="containerRef" flex-1 flex>
+    <div ref="containerRef" style="height: 60%;">
       <div w-full relative flex-1>
         <template v-for="item in nodes" :key="item.id">
           <transition name="slide-fade">
-            <Card
-              v-if="[0, 1].includes(item.state)"
-              :node="item"
-              @click-card="handleSelect"
-            />
+            <Card v-if="[0, 1].includes(item.state)" :node="item" @click-card="handleSelect" />
           </transition>
         </template>
       </div>
       <transition name="bounce">
         <div v-if="isWin" color="#000" flex items-center justify-center w-full text-28px fw-bold>
-          成功加入兔圈~
+          成功~
         </div>
       </transition>
       <transition name="bounce">
@@ -134,21 +130,13 @@ onMounted(() => {
     </div>
 
     <div text-center h-50px flex items-center justify-center>
-      <Card
-        v-for="item in removeList" :key="item.id" :node="item"
-        is-dock
-        @click-card="handleSelectRemove"
-      />
+      <Card v-for="item in removeList" :key="item.id" :node="item" is-dock @click-card="handleSelectRemove" />
     </div>
     <div w-full flex items-center justify-center>
-      <div border="~ 4px dashed #000" w-295px h-44px flex>
+      <div border="~ 4px dashed #000" w-294px h-44px flex>
         <template v-for="item in selectedNodes" :key="item.id">
           <transition name="bounce">
-            <Card
-              v-if="item.state === 2"
-              :node="item"
-              is-dock
-            />
+            <Card v-if="item.state === 2" :node="item" is-dock />
           </transition>
         </template>
       </div>
@@ -156,86 +144,42 @@ onMounted(() => {
 
     <div h-50px flex items-center w-full justify-center>
       <button :disabled="removeFlag" mr-10px @click="handleRemove">
-        移出前三个
+        Move out the first three
       </button>
       <button :disabled="backFlag" @click="handleBack">
-        回退
+        go back
       </button>
     </div>
-    <div w-full color="#000" fw-600 text-center pb-10px>
-      <span mr-20px>designer: Teacher Face</span>
-      by: Xc
-      <a
-        class="icon-btn"
-        color="#000"
-        i-carbon-logo-github
-        rel="noreferrer"
-        href="https://github.com/chenxch"
-        target="_blank"
-        title="GitHub"
-      />
-      <span
-        text-12px
-        color="#00000018"
-      >
-        <span
-          class="icon-btn"
-          text-2
-          i-carbon:arrow-up-left
-        />
-        star buff</span>
-    </div>
-    <audio
-      ref="clickAudioRef"
-      style="display: none;"
-      controls
-      src="./audio/click.mp3"
-    />
-    <audio
-      ref="dropAudioRef"
-      style="display: none;"
-      controls
-      src="./audio/drop.mp3"
-    />
-    <audio
-      ref="winAudioRef"
-      style="display: none;"
-      controls
-      src="./audio/win.mp3"
-    />
-    <audio
-      ref="loseAudioRef"
-      style="display: none;"
-      controls
-      src="./audio/lose.mp3"
-    />
-    <audio
-      ref="welAudioRef"
-      style="display: none;"
-      controls
-      src="./audio/welcome.mp3"
-    />
+    <audio ref="clickAudioRef" style="display: none;" controls src="./audio/click.mp3" />
+    <audio ref="dropAudioRef" style="display: none;" controls src="./audio/drop.mp3" />
+    <audio ref="winAudioRef" style="display: none;" controls src="./audio/win.mp3" />
+    <audio ref="loseAudioRef" style="display: none;" controls src="./audio/lose.mp3" />
+    <audio ref="welAudioRef" style="display: none;" controls src="./audio/welcome.mp3" />
   </div>
 </template>
 
 <style>
-body{
+body {
   background-color: #c3fe8b;
 }
 
 .bounce-enter-active {
   animation: bounce-in 0.5s;
 }
+
 .bounce-leave-active {
   animation: bounce-in 0.5s reverse;
 }
+
 @keyframes bounce-in {
   0% {
     transform: scale(0);
   }
+
   50% {
     transform: scale(1.25);
   }
+
   100% {
     transform: scale(1);
   }
